a18e67d0ee1b19328040204702a5488d8d7ab5d1
Harshad <harshadkhandare9000@gmail.com>
2024-05-08 21:46:26 +0530

    Updated Postgres

--- Databases/Postgres/Intro_2/src/index.ts
+++ Databases/Postgres/Intro_2/src/index.ts
@@ -1,5 +1,5 @@
 import { Client } from 'pg'
-
+// Instead of stsring in below client we can use the variable kinda setting as well (Just for contri)
 const client = new Client({
     connectionString: 'postgresql://Posts_owner:w3XPsqR8coVF@ep-mute-dust-a1u1ith5.ap-southeast-1.aws.neon.tech/Intro?sslmode=require'
 })
