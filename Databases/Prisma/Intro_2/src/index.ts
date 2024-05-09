122281de5d7ac733b77af80e88c604a06ccc3a97
Harshad <harshadkhandare9000@gmail.com>
2024-05-09 21:44:54 +0530

    Autogenerated Clients --> Prisma && Updated Prisma && Updated XLSX

---
+++ Databases/Prisma/Intro_2/src/index.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+
+    const res = await prisma.user.create({
+        data: {
+            username,
+            password,
+            firstName,
+            lastName
+        }
+    })
+
+    console.log(res);
+
+}
+
+insertUser("harshad@gmail.com", "1234", "Harshad", "Khandare");
