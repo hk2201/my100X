5d2e3a05709267cc6055bdf26e91f6b2f6ad98bb
Harshad <harshadkhandare9000@gmail.com>
2024-05-12 21:22:11 +0530

    Updated Postgres assignment

--- Assignments/week-10/1-postgres-simple/src/db/todo.ts
+++ Assignments/week-10/1-postgres-simple/src/db/todo.ts
@@ -1,4 +1,5 @@
 import { client } from "..";
+
 /*
  * Function should insert a new todo for this user
  * Should return a todo object
@@ -11,8 +12,13 @@ import { client } from "..";
  */
 export async function createTodo(userId: number, title: string, description: string) {
     await client.connect();
-
+    const insertQuery = "INSERT INTO todos (userId, title, description) VALUES ($1, $2, $3) RETURNING *;"; // Use RETURNING to get the inserted row
+    const values = [userId, title, description]; // Pass parameters in an array
+    const res = await client.query(insertQuery, values); // Pass query and values to client.query()
+    console.log('Insertion success:', res.rows[0]); // Output inserted todo object
+    return res.rows[0]; // Return the inserted todo object
 }
+
 /*
  * mark done as true for this specific todo.
  * Should return a todo object
@@ -24,7 +30,11 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+    const updateQuery = "UPDATE todos SET done = true WHERE id = $1 RETURNING *;"; // Use RETURNING to get the updated row
+    const values = [todoId]; // Pass parameters in an array
+    const res = await client.query(updateQuery, values); // Execute the update query
+    console.log('Update success:', res.rows[0]); // Output updated todo object
+    return res.rows[0]; // Return the updated todo object
 }
 
 /*
@@ -38,5 +48,9 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
-
+    const selectQuery = "SELECT * FROM todos WHERE userId = $1;"; // Select todos for a specific user
+    const values = [userId]; // Pass parameters in an array
+    const res = await client.query(selectQuery, values); // Execute the select query
+    console.log('Todos:', res.rows); // Output todos array
+    return res.rows; // Return todos array
 }
--- Assignments/week-10/1-postgres-simple/src/db/user.ts
+++ Assignments/week-10/1-postgres-simple/src/db/user.ts
@@ -10,7 +10,12 @@ import { client } from "..";
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+    await client.connect();
+    const insertQuery = "INSERT INTO users (username, password, name) VALUES ($1, $2, $3) RETURNING *;"; // Use RETURNING to get the inserted row
+    const values = [username, password, name]; // Pass parameters in an array
+    const res = await client.query(insertQuery, values); // Pass query and values to client.query()
+    console.log('User created:', res.rows[0]); // Output inserted user object
+    return res.rows[0]; // Return the inserted user object
 }
 
 /*
@@ -22,5 +27,9 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+    const selectQuery = "SELECT * FROM users WHERE id = $1;"; // Select user by ID
+    const values = [userId]; // Pass parameters in an array
+    const res = await client.query(selectQuery, values); // Execute the select query
+    console.log('User:', res.rows[0]); // Output user object
+    return res.rows[0]; // Return user object
 }
