c776633c14fd5aa532d58f4676898c801b78db9c
Harshad Khandare <harshadkhandare9000@gmail.com>
2024-06-17 17:24:13 +0530

    Backend 70 perct

--- Vessel/backend/src/index.ts
+++ Vessel/backend/src/index.ts
@@ -1,79 +1,15 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { userRouter } from './routes/user';
+import { bookRouter } from './routes/blog';
 
-const app = new Hono<{
+export const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
-    JWT_SECRET: string   // IF we dont do this then c.env will complain
+      DATABASE_URL: string;
+      JWT_SECRET: string;
   }
-}>
+}>();
 
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Get particular Route')
-})
-
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('Get Bulk Route')
-})
-
-app.put('/api/v1/blog', (c) => {
-  return c.text("Blog Update Route")
-})
-
-app.post('/api/v1/blog', (c) => {
-  return c.text("Blog Route")
-})
-
-app.post('/api/v1/user/signin', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
-
-  const body = await c.req.json();
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email: body.email
-    }
-  })
-
-  if (!user) {
-    c.status(403);
-    return c.json({ error: "user not found" })
-  }
-
-  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-  return c.json({ jwt });
-})
-
-app.post('/api/v1/user/signup', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
-
-  const body = await c.req.json();
-
-  try {
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password
-      }
-    });
-    const secret = c.env.JWT_SECRET;
-
-    const token = await sign({ id: user.id }, secret);
-
-    return c.json({ token });
-
-  } catch (e) {
-    c.status(403);
-    return c.json({ error: "error while signing up" });
-  }
-})
-
-
+app.route('/api/v1/user', userRouter)
+app.route('/api/v1/book', bookRouter)
 
 export default app
