0df4de452f47efdbee3d5793b4d8a48266ca5a0a
Harshad Khandare <harshadkhandare9000@gmail.com>
2024-06-19 22:23:17 +0530

    Started with Vessel Frontend

---
+++ Vessel/frontend/src/App.tsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Signup } from './Pages/Signup'
+import { Signin } from './Pages/Signin'
+import { Blog } from './Pages/Blog'
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/blog/:id' element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
