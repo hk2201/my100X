eb78179965d28ce837520635f820934eb44d610d
Harshad Khandare <harshadkhandare9000@gmail.com>
2024-06-30 22:00:40 +0530

    Frontend Bug fixes and updates

--- Vessel/frontend/src/Components/Quote.tsx
+++ Vessel/frontend/src/Components/Quote.tsx
@@ -4,7 +4,7 @@ export const Quote = () => {
         <div className="flex justify-center">
             <div className="max-w-lg">
                 <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                    "The customer support I received is exceptional. The support team went above and beyond to address my concerns"
                 </div>
                 <div className="max-w-md text-xl font-semibold text-left mt-4">
                     Julies Winfield
