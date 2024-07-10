c801ab145399c30ae547ac4ea1ad31e1b44f239d
Harshad Khandare <harshadkhandare9000@gmail.com>
2024-07-10 22:44:42 +0530

    Started Next Js and updated the Readme File

---
+++ Next_Js/Readme.md
@@ -0,0 +1,2 @@
+1. npx create-next-app@latest --> command to setup next js project
+2. NExt has Files based routing 
---
+++ Next_Js/next/app/signup/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+    return (
+        <div className="flex bg-slate-400 h-screen justify-center items-center">
+            Signup Page
+        </div>
+    );
+}
