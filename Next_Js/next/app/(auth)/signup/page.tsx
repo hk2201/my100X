38000609cef04b7193261f63fe6d1bd6a7ebd4d2
Harshad Khandare <harshadkhandare9000@gmail.com>
2024-07-12 22:02:45 +0530

    Created Separate Component for Sign Up

--- Next_Js/next/app/(auth)/signup/page.tsx
+++ Next_Js/next/app/(auth)/signup/page.tsx
@@ -1,9 +1,10 @@
+import { SignupComponent } from "@/components/Signup";
 
 
 export default function Home() {
     return (
         <div className="flex bg-slate-400 h-screen justify-center items-center">
-            Signup Page
+            <SignupComponent />
         </div>
     );
 }
