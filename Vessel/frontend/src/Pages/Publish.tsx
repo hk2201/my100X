2262a66978e4f6e24c3a9c4c15c4fa16e5ffd9fa
Harshad Khandare <harshadkhandare9000@gmail.com>
2024-07-14 13:57:04 +0530

    Updated Publish

--- Vessel/frontend/src/Pages/Publish.tsx
+++ Vessel/frontend/src/Pages/Publish.tsx
@@ -44,7 +44,7 @@ function TextEditor({ onChange }: { onChange: (e: ChangeEvent<HTMLTextAreaElemen
         <div className="w-full mb-4 ">
             <div className="flex items-center justify-between border">
                 <div className="my-2 bg-white rounded-b-lg w-full">
-                    <label className="sr-only">Publish post</label>
+                    <label className="sr-only">Publish your post</label>
                     <textarea onChange={onChange} id="editor" rows={8} className="focus:outline-none block w-full px-0 text-sm text-gray-800 bg-white border-0 pl-2" placeholder="Write an article..." required />
                 </div>
             </div>
