019ba942d09faca7ae79a5b58ec6338d09e25634
Harshad Khandare <harshadkhandare9000@gmail.com>
2024-07-21 20:57:46 +0530

    Modification for Typescript Types

--- Typescript/Types/types.ts
+++ Typescript/Types/types.ts
@@ -1,12 +1,37 @@
-// They are very similar to interfaces just that there are some tweeks in their use cases
+// Example 1: Unions
 
-// Example 1 . Unions
+type StringOrNumber = string | number;
 
-type StringorNumber = string | number;
+type IsBoolean = boolean;
 
-function print(num: StringorNumber) {
+// Corrected type definition for an array of strings
+type IsArray = string[];
+
+function print(num: StringOrNumber) {
     console.log(num);
+}
+
+function print2(name: StringOrNumber) {
+    console.log(name);
+}
+
+function print3(value: IsBoolean) {
+    console.log(value);
+}
+
+// Corrected print4 function to work with an array of strings
+function print4(arr: IsArray) {
+    arr.map((e) => {
+        console.log(e);
+    });
 }
 
 print(100);
 print("100");
+
+print2("Harshad Khandare");
+
+print3(false);
+
+// Updated the argument to match the IsArray type
+print4(["harshad", "khandare", "GOAT"]);
