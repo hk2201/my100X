8a185fe8595dd2d27ace83ed801d2cf355fdf56a
Harshad Khandare <harshadkhandare9000@gmail.com>
2024-08-03 17:55:01 +0530

    Javascript Changes with newly added functions

--- Javascript/Classes.js
+++ Javascript/Classes.js
@@ -1,20 +1,30 @@
 class Animal {
-    constructor(name,legs,color){
-        this.name = name;
-        this.legs = legs;
-        this.color = color;
-    }
+  constructor(name, legs, color) {
+    this.name = name;
+    this.legs = legs;
+    this.color = color;
+  }
 
-    // will not be associated with any object;
-    static myfunc(){
-        console.log("Not associated with object");
-    }
-    speak(){
-        console.log("Bhow Bhow");
-    }
+  // will not be associated with any object;
+  static myfunc() {
+    console.log("Not associated with object");
+  }
+  speak() {
+    console.log("Bhow Bhow");
+  }
+
+  walk() {
+    console.log("I am walking");
+  }
+
+  sleep() {
+    console.log("Going into Sleep for 1200 ms");
+  }
 }
 
-let dog = new Animal("dog",4,"black");
+let dog = new Animal("dog", 4, "black");
 
 dog.speak();
+dog.walk();
+dog.sleep();
 console.log(dog.name);
--- Javascript/JSON.js
+++ Javascript/JSON.js
@@ -1,14 +1,21 @@
 // Below is an object
 const users = {
-    myName : "harsad",
-    gender : "male",
-    height : 170
+  myName: "harsad",
+  gender: "male",
+  height: 170,
+};
 
-}
+const users2 = {
+  myName: "Macintosh",
+  gender: "Female",
+  height: 120,
+};
 
 // so while sending data we stringy so that universally anyone can understand
 const user = JSON.stringify(users);
 console.log(user);
+
+const user2 = JSON.stringify(users2);
 
 //to convert the string to JSON
 
