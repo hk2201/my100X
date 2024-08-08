2612478b9e11fefa5ea9ae64a6c9c8073e4f22ae
Harshad Khandare <harshadkhandare9000@gmail.com>
2024-08-08 21:23:14 +0530

    Changes in Javascript Functions

--- Javascript/Map.js
+++ Javascript/Map.js
@@ -1,7 +1,23 @@
-const arr = [1,2,3,4,5,6];
+const arr = [1, 2, 3, 4, 5, 6];
 
-const newarr = arr.map(i => {
-    return i*2;
-})
+const newarr = arr.map((i) => {
+  return i * 2;
+});
+
+const add = arr.map((i) => {
+  return i + 2;
+});
+
+const subs = arr.map((i) => {
+  return i - 2;
+});
+
+const mult = arr.map((i) => {
+  return 1 * 2 * 3;
+});
+
+const modd = arr.map((i) => {
+  return i % 2;
+});
 
 console.log(newarr);
